540344454c2a57542f54b092e55c5e2062066847
zhaoshuai6 <469182582@qq.com>
2021-01-04 18:12:19 +0800

    feign方式调用接口测试

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,13 +1,23 @@
 package com.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/test")
     public String hello(){
         return "test";
+    }
+
+    @GetMapping("/client/{id}")
+    public String feignTest(@PathVariable("id") Integer id){
+        return "test:" + port + ":" + id;
     }
 }
--- eureka-customer/src/main/java/com/example/eurekacustomer/controller/TestController.java
+++ eureka-customer/src/main/java/com/example/eurekacustomer/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.example.eurekacustomer.controller;
 
 
+import com.example.eurekacustomer.client.CustomerClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,11 +21,19 @@ public class TestController {
     @Autowired
     private LoadBalancerClient loadBalancerClient;
 
+    @Autowired
+    private CustomerClient customerClient;
+
     @GetMapping("/customer/test")
     public String test(){
         ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
         System.out.println(serviceInstance);
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/test" ;
         return restTemplate.getForObject(url,String.class);
+    }
+
+    @GetMapping("/customer/client/{id}")
+    public String feignTest(@PathVariable("id") Integer id){
+        return customerClient.feignTest(id);
     }
 }
