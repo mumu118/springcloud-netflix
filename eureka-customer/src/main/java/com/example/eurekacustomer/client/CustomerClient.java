f119f9f07eb5082758da6c8c920eda8206c955b4
zhaoshuai6 <469182582@qq.com>
2021-01-05 10:04:08 +0800

    feign负载均衡策略配置

--- eureka-customer/src/main/java/com/example/eurekacustomer/client/CustomerClient.java
+++ eureka-customer/src/main/java/com/example/eurekacustomer/client/CustomerClient.java
@@ -1,13 +1,12 @@
 package com.example.eurekacustomer.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name="eureka-client")
 public interface CustomerClient {
 
-    @RequestMapping(value="/client/{id}",method = RequestMethod.GET)
-    public String feignTest(@PathVariable("id") Integer id);
+    @GetMapping(value="/client/{id}")
+    String feignTest(@PathVariable("id") Integer id);
 }
